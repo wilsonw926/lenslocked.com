2d9db9e54956dd2d3ec8e9cd1831bc13d3ea0474
Wilson Wang <wilsonw926@berkeley.edu>
2020-07-09 22:52:18 -0700

    add contact page

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "<html>To get in touch, email me at <a href=\"mailto:test@gmail.com\">test@gmail.com</a>.</html>")
+	}
 }
 
 func main() {
